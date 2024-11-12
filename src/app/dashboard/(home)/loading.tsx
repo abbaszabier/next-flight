aac7e61d260ff3ce126357148f8e02ab6cc4a8f5
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2024-11-12 10:34:23 +0700

    feat: UI Loading Skeleton

--- src/app/dashboard/(home)/loading.tsx
+++ src/app/dashboard/(home)/loading.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function loading() {
-  return <div>Loading...</div>;
+  return (
+    <div className="flex flex-col space-y-3">
+      <div className="flex justify-between">
+        <Skeleton className="h-10 w-[160px]" />
+        <Skeleton className="h-10 w-[120px]" />
+      </div>
+      <Skeleton className="h-[160px] w-full rounded-xl" />
+    </div>
+  );
 }
