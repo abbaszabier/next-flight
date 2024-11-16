c7b8eb782a73c124c058437351c08e781fa0fea1
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2024-11-16 09:06:20 +0700

    fix: UI

--- src/app/dashboard/(home)/components/buttonLogout.tsx
+++ src/app/dashboard/(home)/components/buttonLogout.tsx
@@ -16,7 +16,7 @@ const Logout: React.FC = () => {
       type="submit"
       variant="unstyled"
       size="icon"
-      className="w-full flex justify-start px-2 hover:bg-gray-100"
+      className="w-full flex justify-start px-2 hover:bg-accent"
     >
       {pending ? (
         <div className="flex gap-2 items-start justify-start">
@@ -24,7 +24,7 @@ const Logout: React.FC = () => {
         </div>
       ) : (
         <>
-          <LogOut />
+          <LogOut className="w-5 h-5 mr-1" size={18} />
           Logout
         </>
       )}
